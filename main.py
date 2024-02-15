597a99658b450befd28d4a4a484ed0394fc82e58
WORK_user <www.pavel_ru.ru@mail.ru>
2024-02-15 17:05:06 +0300

    Сделан приятный вид кода.

--- main.py
+++ main.py
@@ -24,8 +24,11 @@ finish_amo_leads = datetime.strptime(amocrm_data.finish_amo_leads, "%Y-%m-%d").t
 start_amo_events = datetime.strptime(amocrm_data.start_amo_events, "%Y-%m-%d").timestamp()
 finish_amo_events = datetime.strptime(amocrm_data.finish_amo_events, "%Y-%m-%d").timestamp()
 
-link_leads = f'https://{amocrm_data.subdomain}.amocrm.ru/api/v4/leads?filter[created_at][from]={start_amo_leads}&filter[created_at][to]={finish_amo_leads}'
-link_events = f'https://{amocrm_data.subdomain}.amocrm.ru/api/v4/events?filter[entity][]=lead&filter[type]=lead_status_changed&filter[created_at][from]={start_amo_events}&filter[created_at][to]={finish_amo_events}'
+link_leads = (f'https://{amocrm_data.subdomain}.amocrm.ru/api/v4/leads?filter[created_at][from]={start_amo_leads}'
+              f'&filter[created_at][to]={finish_amo_leads}')
+link_events = (f'https://{amocrm_data.subdomain}.amocrm.ru/api/v4/events?filter[entity][]=lead&filter['
+               f'type]=lead_status_changed&filter[created_at][from]={start_amo_events}&filter[c'
+               f'reated_at][to]={finish_amo_events}')
 data_headers = {"Authorization": 'Bearer ' + amocrm_data.access_token}
 
 link_postgres = f'postgresql://{db.user_db}:{db.password_db}@{db.address_db}:{db.port_db}/{db.name_db}'
@@ -34,14 +37,14 @@ engine = create_engine(link_postgres)
 
 def run():
     # AmoCRM
-    # logger.warning("Начался процесс выгрузки данных из AmoCRM системы!")
-    # df_leads = get_leads(link_leads, data_headers, amocrm_data.columns_leads)
-    # df_events = get_events(link_events, data_headers, dict_events, amocrm_data.columns_events)
-    # if df_leads is not None and df_events is not None:
-    #     merge_tables(engine, df_leads, df_events)
-    # else:
-    #     # Обработка ситуации, если один из DataFrame равен None
-    #     logger.warning("Ошибка при получении данных из API или чтении файлов!")
+    logger.warning("Начался процесс выгрузки данных из AmoCRM системы!")
+    df_leads = get_leads(link_leads, data_headers, amocrm_data.columns_leads)
+    df_events = get_events(link_events, data_headers, dict_events, amocrm_data.columns_events)
+    if df_leads is not None and df_events is not None:
+        merge_tables(engine, df_leads, df_events)
+    else:
+        # Обработка ситуации, если один из DataFrame равен None
+        logger.warning("Ошибка при получении данных из API или чтении файлов!")
 
     # Bitrix24
     logger.warning("Начался процесс выгрузки данных из Bitrix24 системы!")
@@ -51,22 +54,22 @@ def run():
               required_columns=bitrix24_data.columns_bitrix24, replace_dict=dict_bitrix24)
 
     # Yandex
-    # logger.warning("Начался процесс выгрузки данных из Yandex системы!")
-    # tokens = yandex_data.tokens
-    # logins = yandex_data.logins
-    # df_yandex = pd.DataFrame()
-    # for token, login in zip(tokens, logins):
-    #     headers = create_headers(token, login)
-    #     df1 = yandex(yandex_data.reports_url, body, headers)
-    #     df_yandex = pd.concat([df_yandex, df1])
-    #     df_yandex = rename_conversions(df_yandex)
-    # try:
-    #     logger.warning("Начался процесс выгрузки данных из Yandex системы!")
-    #     df_yandex = df_yandex[df_yandex['Cost'] != 0]
-    #     yandex_to_database(engine, df_yandex)
-    #     logger.info("Выгрузка данных Yandex прошла успешно!")
-    # except Exception as warning:
-    #     logger.warning(f'Строки колонки Cost не были удалены. Код предупреждения - {warning}. Выгрузка не удалась!')
+    logger.warning("Начался процесс выгрузки данных из Yandex системы!")
+    tokens = yandex_data.tokens
+    logins = yandex_data.logins
+    df_yandex = pd.DataFrame()
+    for token, login in zip(tokens, logins):
+        headers = create_headers(token, login)
+        df1 = yandex(yandex_data.reports_url, body, headers)
+        df_yandex = pd.concat([df_yandex, df1])
+        df_yandex = rename_conversions(df_yandex)
+    try:
+        logger.warning("Начался процесс выгрузки данных из Yandex системы!")
+        df_yandex = df_yandex[df_yandex['Cost'] != 0]
+        yandex_to_database(engine, df_yandex)
+        logger.info("Выгрузка данных Yandex прошла успешно!")
+    except Exception as warning:
+        logger.warning(f'Строки колонки Cost не были удалены. Код предупреждения - {warning}. Выгрузка не удалась!')
 
 
 if __name__ == "__main__":
