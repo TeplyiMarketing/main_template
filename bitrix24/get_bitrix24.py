cf501c4a8e55433dbaa13ec8852932a0571a19ff
WORK_user <www.pavel_ru.ru@mail.ru>
2024-03-06 17:36:11 +0300

    Сделана разбивка bitrix24.

--- bitrix24/get_bitrix24.py
+++ bitrix24/get_bitrix24.py
@@ -16,41 +16,80 @@ def get_deals(bitrix24, engine, params, required_columns, replace_dict):
                     logger.warning('Ключ не найден - пропускаем.Функция Bitrix24')
             if 'UTM_CONTENT' in required_columns:
                 def split_utm_content(df):
-                    # Находим первую непустую строку для определения названий колонок
-                    first_non_empty = df['UTM_CONTENT'].dropna().iloc[0]
+                    logger.debug("Начало обработки данных")
 
-                    if '//' in first_non_empty:
-                        delimiter = '//'
-                    elif '||' in first_non_empty:
-                        delimiter = '||'
-                    else:
-                        raise ValueError("Ни один из ожидаемых разделителей не найден.")
-                    # Разбиваем её для создания списка названий колонок
-                    column_names = [f'utm_content_{item.split(":")[0]}' for item in first_non_empty.split(delimiter)]
+                    # Явно преобразуем все значения в столбце 'UTM_content' в строки
+                    df['UTM_CONTENT'] = df['UTM_CONTENT'].apply(lambda x: str(x))
 
-                    # Разделяем столбец на несколько столбцов
-                    utm_df = df['UTM_CONTENT'].str.split('//', expand=True)
+                    # Определяем строки, содержащие 'groupid'
+                    contains_groupid = df['UTM_CONTENT'].str.contains('groupid')
+                    logger.debug(f"Строки, содержащие 'groupid': {df[contains_groupid].index.tolist()}")
 
-                    # Обрезаем лишние столбцы если они есть
-                    utm_df = utm_df.iloc[:, :len(column_names)]
+                    if not contains_groupid.any():
+                        logger.warning("Строк с 'groupid' не найдено. Пропуск разделения.")
+                        return df
 
-                    # Присваиваем названия новым столбцам
-                    utm_df.columns = column_names
+                    first_non_empty = df[contains_groupid]['UTM_CONTENT'].iloc[0]
+                    delimiter = '||' if '||' in first_non_empty else '//'
+                    logger.info(f"Используемый разделитель: '{delimiter}'")
 
-                    # Разделяем значения в каждом из новых столбцов по ':'
-                    for col in column_names:
-                        utm_df[col] = utm_df[col].str.split(':').str[1]
+                    column_names = [f'UTM_CONTENT_{item.split(":")[0].strip()}' for item in
+                                    first_non_empty.split(delimiter)]
 
-                    # Добавляем новые столбцы обратно в исходный dataframe
-                    df = pd.concat([df.drop('UTM_CONTENT', axis=1), utm_df], axis=1)
+                    # Создаем список словарей для новых данных
+                    utm_data_list = []
+                    for idx, content in df[contains_groupid].iterrows():
+                        data_dict = {}
+                        for item in content['UTM_CONTENT'].split(delimiter):
+                            if ':' in item:
+                                key, value = item.split(':', 1)
+                                data_dict[f'utm_content_{key.strip()}'] = value.strip()
+                        utm_data_list.append(data_dict)
+
+                    utm_data = pd.DataFrame(utm_data_list)
+
+                    df = pd.concat(
+                        [df.drop('UTM_CONTENT', axis=1).reset_index(drop=True), utm_data.reset_index(drop=True)],
+                        axis=1)
+                    logger.info("Конкатенация новых столбцов с исходным DataFrame выполнена.")
 
                     return df
 
                 try:
-                    # df_deals - ваш исходный DataFrame
                     df_deals = split_utm_content(df_deals)
                 except Exception as error:
-                    logger.warning(f'Отсутствует {error}.')
+                    logger.warning(f'Ошибка разделения -  {error}.')
+
+                if 'UTM_CAMPAIGN' in required_columns:
+                    def split_utm_campaign_and_insert(df):
+                        if 'UTM_CAMPAIGN' not in df.columns:
+                            print("Столбец 'UTM_CAMPAIGN' не найден в DataFrame.")
+                            return df
+
+                        # Извлекаем ID из столбца 'UTM_campaign'
+                        df['UTM_CAMPAIGN_ID'] = df['UTM_CAMPAIGN'].str.split('-').str[-1]
+
+                        # Преобразуем 'UTM_campaign_id' в числовой формат
+                        df['UTM_CAMPAIGN_ID'] = pd.to_numeric(df['UTM_CAMPAIGN_ID'], errors='coerce')
+
+                        # Находим индекс столбца 'UTM_campaign'
+                        campaign_index = df.columns.get_loc('UTM_CAMPAIGN') + 1
+
+                        # Создаем копию столбца 'UTM_campaign_id'
+                        campaign_id_column = df['UTM_CAMPAIGN_ID']
+
+                        # Удаляем 'UTM_campaign_id' из текущего положения
+                        df.drop('UTM_CAMPAIGN_ID', axis=1, inplace=True)
+
+                        # Вставляем 'UTM_campaign_id' рядом со столбцом 'UTM_campaign'
+                        df.insert(campaign_index, 'UTM_CAMPAIGN_ID', campaign_id_column)
+
+                        return df
+
+                    try:
+                        df_deals = split_utm_campaign_and_insert(df_deals)
+                    except Exception as error:
+                        logger.warning(f'Ошибка разделения -  {error}.')
 
             if removed_columns:
                 logger.info(f'Удалены следующие столбцы: {", ".join(removed_columns)}. Функция Bitrix24.')
@@ -58,10 +97,6 @@ def get_deals(bitrix24, engine, params, required_columns, replace_dict):
                 logger.info('Нет столбцов для удаления. Функция Bitrix24.')
         else:
             logger.warning('Отсутствует колонка.')
-        # try:
-        #     df_deals = split_utm_content(df_deals)
-        # except Exception as error:
-        #     logger.error(error)
         # Проверьте, есть ли сделка уже в базе данных
         df_deals['STAGE_ID'] = df_deals['STAGE_ID'].replace(replace_dict)
         df_deals['MOVED_TIME'] = pd.to_datetime(df_deals['MOVED_TIME']).dt.date
